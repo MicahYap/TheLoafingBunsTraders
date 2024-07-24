65f019888036c3921aaef574bdb1f34205b0b12f
micahyap <yapmicahsy@gmail.com>
2024-07-24 16:24:06 +0800

    added logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
+  root 'secured_assets#index'
+
   resources :secured_assets
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
